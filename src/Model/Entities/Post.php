2c899beddd32512ca36cd65916d8df2564587bfc
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2020-05-15 01:01:36 +0200

    Add Post entity

---
+++ src/Model/Entities/Post.php
@@ -0,0 +1,33 @@
+<?php
+namespace Model\Entities;
+
+class Post {
+  private $id;
+  private $title;
+  private $author;
+  private $date;
+
+
+  public function getId() {
+    return $this->id;
+  }
+
+  public function getTitle() {
+    return $this->title;
+  }
+
+  public function getAuthor() {
+    return $this->author;
+  }
+
+  public function getDate() {
+    return $this->date;
+  }
+
+  public function __construct($id, $title, $author, $date) {
+    $this->id = $id;
+    $this->title = $title;
+    $this->author = $author;
+    $this->date = $date;
+  }
+}
--- src/Services/MockRepository.php
+++ src/Services/MockRepository.php
@@ -10,6 +10,11 @@ class MockRepository implements \Model\Interfaces\Repository {
       array('1', 'jeff', 'jeff123'),
       array('2', 'lisa', 'lisa123')
     );
+
+    $this->mockPosts = array(
+      array('1', 'Lasfuegos1', 'May 13 at 16:14 PM', 'Tips and tricks for new people', '2 mins', 'flyredpanda'),
+      array('2', 'Coolguy98', 'May 12 at 11:20 AM', 'I want to go one step further', '45 mins', 'RishabhJet')
+    );
   }
 
   public function getUserForUserNameAndPassword(string $userName, string $password): ?\Model\Entities\User {
